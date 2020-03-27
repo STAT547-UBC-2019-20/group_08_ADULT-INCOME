3791eef8434d3936a27b9328a146ae79d1fd2656
jimmyliu1326 <jimmyliu1326@yahoo.com>
2020-03-27 15:39:01 -0700

    updated app.R to contain fully fleshed out layout created dash_components.R and dash_functions.R to create graphs and accessory components added css assests for prettier app layout

--- app.R
+++ app.R
@@ -10,24 +10,86 @@ Usage: app.R
 library(dash)
 library(dashCoreComponents)
 library(dashHtmlComponents)
+library(dashTable)
+suppressPackageStartupMessages(library(here))
+
+# load data
+dat <- read.csv(here("data/processed_adult-data.csv"), header = T)
+
+# source
+source(here("dash_functions.R"))
+source(here("dash_components.R"))
+
+
 
 
 # Create Dash instance
 app <- Dash$new()
 
-# 3. Specify App layout
+# Specify App layout
 app$layout(
-  htmlDiv(
-    list(
-      htmlH1('1994 ADULT INCOME CENSUS')
-    )
-  )
+      title,
+      instructions,
+      htmlDiv( # content board
+        list(
+          htmlDiv( # top component
+            list(
+              demographics_header,
+              htmlDiv( # top component contents
+                list(
+                  htmlDiv( # top component side
+                    list(
+                      top_sidebar,
+                      htmlDiv(
+                        className = "pretty_container",
+                        list(
+                          htmlH3("Financial Summary"),
+                          table
+                        ), style = list("height"=260,
+                                        "width"="100%")
+                      )
+                    ), style = list("width"="20%",
+                                    "height"="100%")),
+                  htmlDiv( # top component graph
+                    className = "pretty_container",
+                    distribution,
+                    style = list("width"="80%",
+                                 "height"="100%",
+                                 'margin-left'=50)
+                  )
+                ), style = list('display'= 'flex',
+                                'height'="100%")
+                )
+              ), style = list('height'="50%")
+            ),
+          htmlDiv( # bottom component
+            list(
+              analytics_header,
+              htmlDiv( # bottom component contents
+                list(
+                  bottom_sidebar,
+                  htmlDiv( # bottom component plots
+                        className = "pretty_container",
+                        list(empty), 
+                        style = list('width'='80%')
+                        )
+                    ), style = list('display'='flex',
+                                    'width'='100%',
+                                    'height'="100%")
+                )
+              ), style = list('width'='100%',
+                              'height'='50%')
+            )
+          ), style = list('display' = 'block',
+                          'margin-left' = 'auto',
+                          'margin-right' = 'auto',
+                          'width'='79.5%',
+                          'height'='80%')
+        ) 
 )
+# Run app
 
-
-# 4. Run app
-
-app$run_server(debug=TRUE)
+app$run_server(debug = TRUE)
 
 # command to add dash app in Rstudio viewer:
 # rstudioapi::viewer("http://127.0.0.1:8050")
---
+++ dash_components.R
@@ -0,0 +1,113 @@
+# title
+title <- htmlDiv(
+  className = "pretty_container",
+  list(htmlH1('1994 ADULT INCOME CENSUS')),
+  style = list(
+    'columnCount' = 1,
+    'background-color' = 'lightblue',
+    'text-align' = 'center',
+    'height'=75
+    )
+)
+
+# app instructions
+instructions <- htmlDiv(
+  className = "pretty_container",
+  list(
+    htmlH2("Instructions"),
+    dccMarkdown(
+"
+Welcome to the dashboard developed by Group 8!
+        
+Blah..Blah..Blah,
+Blah..Blah..Blah
+"
+              ))
+)
+
+# demographics header
+demographics_header <- htmlDiv(
+  className = "pretty_container",
+  list(htmlH2("Demographics Overview")),
+  style = list('background-color'='',
+               'height'=30
+               )
+)
+
+# analytics header
+analytics_header <- htmlDiv(
+  className = "pretty_container",
+  list(htmlH2("Analytics")),
+  style = list('background-color'='',
+               'height'=30,
+               'vertical-align'='middle')
+)
+
+# dropdown
+dropdownkey <-
+  tibble(
+    label = c("Sex", "Age", "Work Class", "Education", "Martial Status", "Race", "Native Country"),
+    value = c("sex", "age", "workclass", "education", "martial_status", "race", "native_country")
+  )
+dropdown <- dccDropdown(
+  id = "dropdown",
+  options = map(1:nrow(dropdownkey), function(i) {
+    list(label = dropdownkey$label[i], value = dropdownkey$value[i])
+  }),
+  value = "sex" # set default value
+)
+
+place_holder <- dccDropdown(
+  id = "place_holder",
+  options = map(1:nrow(dropdownkey), function(i) {
+    list(label = dropdownkey$label[i], value = dropdownkey$value[i])
+  }),
+  value = "sex" # set default value
+)
+
+# sidebars
+top_sidebar <- htmlDiv(
+      className = "pretty_container",
+      list(
+        htmlP("Select the distribution plot variable:"),
+        dropdown,
+        htmlBr()
+      ), style = list('columnCount' = 1,
+                      'height'=150,
+                      'width'='100%',
+                      'white-space' = 'pre-line')
+)
+
+bottom_sidebar <- htmlDiv(
+  className = "pretty_container",
+  list(
+    htmlP("Select the plot variable:"),
+    place_holder,
+    htmlBr()
+    ), style = list('columnCount' = 1,
+                    'height'=475,
+                    'width'="20.5%",
+                    'white-space' = 'pre-line')
+)
+
+# distribution plot
+distribution <- dccGraph(id = "distribution",
+                         figure = make_distribution())
+
+# empty plot
+empty <- dccGraph(id = "empty",
+                         figure = make_empty())
+
+# summary table
+table <- dashDataTable(
+  id = "table",
+  columns = lapply(colnames(make_table()), 
+                   function(colName){
+                     list(
+                       id = colName,
+                       name = colName
+                     )
+                   }),
+  data = df_to_list(make_table()),
+  style_table = list('height'='auto')
+)
